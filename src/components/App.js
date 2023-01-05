9d878119dab8c004eee222969f8dd790ec4d9677
Danico-mfala <mfaladan@hotmail.com>
2023-01-05 14:04:14 +0300

    initialise to git

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2>Surreal estate</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
